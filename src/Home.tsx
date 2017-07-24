5947e3c1b19f3194740940153f27ada140643e7d
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-07-23 23:03:29 -0400

    Add button to start new game

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { Container, Button, Divider } from 'semantic-ui-react';
+import * as shortid from 'shortid';
+
+import 'semantic-ui-css/semantic.min.css';
+
 import './Home.css';
 
 interface HomeProps extends RouteComponentProps<{}> {}
 
 class Home extends React.Component<HomeProps, {}> {
+    onNewGameClick() {
+        const newSeed = shortid.generate();
+
+        this.props.history.push('/g/' + newSeed);
+    }
+
     render() {
         return (
             <div className = "Home">
-                Hello, world!
+                <Container text = {true}>
+                Welcome to Seppuku! We offer an exciting selection of billions of randomly-generated puzzles!
+
+                <br />
+
+                Please click below to start. The links to the puzzles are permanent, so you can share with friends!
+
+                <Divider />
+
+                <Container textAlign = "center">
+                    <Button size = "huge" color = "violet" onClick = {() => this.onNewGameClick()}>New Game</Button>
+                </Container>
+
+                </Container>
             </div>
         );
     }
