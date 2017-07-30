1e49b6d50e71d9612b927e9422a49b28bb9e085c
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-07-30 18:55:50 -0400

    Implement clipboard copy

--- src/ShareButton.tsx
+++ src/ShareButton.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Button } from 'semantic-ui-react';
+import * as CopyToClipboard from 'react-copy-to-clipboard';
 
 import 'semantic-ui-css/semantic.min.css';
 import './ShareButton.css';
@@ -7,7 +8,9 @@ import './ShareButton.css';
 class ShareButton extends React.Component<{}, {}> {
     render() {
         return (
-            <Button className = "sharingBtn icon" color = "violet" size = "huge" icon = "share alternate" />
+            <CopyToClipboard text = {window.location.href}>
+                <Button className = "sharingBtn icon" color = "violet" size = "huge" icon = "share alternate" />
+            </CopyToClipboard>
         );
     }
 }
